f72e61e98ab56b9f2fc82c2f72c3b0d6863e180d
Drew Cook <dc333@usf.edu>
2025-02-16 19:36:45 -0500

    Task 2 - Product Price After Discount.

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) { //declare function with p
 
 console.log(calculateSalary(5000, 500, 0.1)); //test data
 console.log(calculateSalary(7000, 1000, 0.15));
+
+// Task 2: function expression. Product price after discount.
+const calculateDiscount = function(price, discountRate) { //declare function
+    finalPrice = price - (price * discountRate); //use formula to find final price after discount
+    return `Final Price: $${finalPrice.toFixed(2)}`; //return a string including the final price to two decimal places
+}
+
+console.log(calculateDiscount(100, 0.2)); //test data
+console.log(calculateDiscount(250, 0.15));
