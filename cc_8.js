77a4de119c3d51b4b7758283e81313a3cd4f0a5d
Drew Cook <dc333@usf.edu>
2025-02-16 19:49:32 -0500

    Task 3 - Service Fee Calculation.

--- cc_8.js
+++ cc_8.js
@@ -1,13 +1,13 @@
 // Task 1: function declaration. Employee salary calculation scenario.
 function calculateSalary(baseSalary, bonus, taxRate) { //declare function with parameters
     netSalary = (baseSalary + bonus) - (baseSalary * taxRate); //use formula to calculate the net salary
-    return `Net Salary: $${netSalary.toFixed(2)}` //return the net salary as an amount to 2 decimal places
+    return `Net Salary: $${netSalary.toFixed(2)}`; //return the net salary as an amount to 2 decimal places
 }
 
 console.log(calculateSalary(5000, 500, 0.1)); //test data
 console.log(calculateSalary(7000, 1000, 0.15));
 
-// Task 2: function expression. Product price after discount.
+// Task 2: function expression. Product price after discount scenario.
 const calculateDiscount = function(price, discountRate) { //declare function
     finalPrice = price - (price * discountRate); //use formula to find final price after discount
     return `Final Price: $${finalPrice.toFixed(2)}`; //return a string including the final price to two decimal places
@@ -15,3 +15,20 @@ const calculateDiscount = function(price, discountRate) { //declare function
 
 console.log(calculateDiscount(100, 0.2)); //test data
 console.log(calculateDiscount(250, 0.15));
+
+// Task 3: arrow function. Service fee calculation scenario.
+const calculateServiceFee = (amount, serviceType) => {
+    if (serviceType === "Premium") {
+        fee = amount * 0.15;
+    } else if (serviceType === "Standard") {
+        fee = amount * 0.1;
+    } else {
+        fee = amount = 0.05;
+    };
+
+    return `Service Fee: $${fee.toFixed(2)}`;
+    }; 
+
+
+console.log(calculateServiceFee(200, "Premium")); // Expected output: "Service Fee: $30.00"
+console.log(calculateServiceFee(500, "Standard"));
