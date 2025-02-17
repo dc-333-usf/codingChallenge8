ff58a78ea15956e2ae81b6252d9779d8c84104ae
Drew Cook <dc333@usf.edu>
2025-02-16 19:36:20 -0500

    Task 1 - Employee Salary Calculation.

--- cc_8.js
+++ cc_8.js
@@ -1,8 +1,8 @@
 // Task 1: function declaration. Employee salary calculation scenario.
-function calculateSalary(baseSalary, bonus, taxRate) {
-    netSalary = (baseSalary + bonus) - (baseSalary * taxRate);
-    return `Net Salary: $${netSalary.toFixed(2)}`
+function calculateSalary(baseSalary, bonus, taxRate) { //declare function with parameters
+    netSalary = (baseSalary + bonus) - (baseSalary * taxRate); //use formula to calculate the net salary
+    return `Net Salary: $${netSalary.toFixed(2)}` //return the net salary as an amount to 2 decimal places
 }
 
-console.log(calculateSalary(5000, 500, 0.1));
+console.log(calculateSalary(5000, 500, 0.1)); //test data
 console.log(calculateSalary(7000, 1000, 0.15));
